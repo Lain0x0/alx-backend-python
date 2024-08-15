ac8de5f0a296c9009674f40a90f478c8b9ceb76a
Lain0x0 <lainyamato03@gmail.com>
2024-08-15 21:02:57 +0000

    0x6-sum_mixed

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 6-complexe mixed list """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ A function takes a list of ints and floats and return the sum """
+    return sum(mxd_lst)
