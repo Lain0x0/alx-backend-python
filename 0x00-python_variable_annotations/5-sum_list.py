b4a02ffcd763d112f076b366a8d5d3c62fc6de55
Lain0x0 <lainyamato03@gmail.com>
2024-08-15 19:59:29 +0000

    0x5-sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 5-complexe list """
+from typing import List
+
+
+def sum_list(input_list: List[(float)]) -> float:
+    """ A function takes a list of floats and return their sum of it """
+    return sum(input_list)
