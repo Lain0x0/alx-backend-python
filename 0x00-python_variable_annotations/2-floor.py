7c6016a72aa6a41893c353802fac6342f02c72b7
Lain0x0 <lainyamato03@gmail.com>
2024-08-15 19:44:42 +0000

    0x2-floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 2-floor basic math using math module """
+import math
+
+
+def floor(n: float) -> int:
+    """ A function that return the floor float """
+    return math.floor(n)
