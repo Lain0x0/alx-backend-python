5031036c53975596b1e6b2641492d8becae02acd
Lain0x0 <lainyamato03@gmail.com>
2024-09-06 15:37:36 +0000

    0x3-test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+        ])
     def test_access_nested_map(
             self,
             nested_map: Dict,
@@ -26,7 +26,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
-    ])
+        ])
     def test_access_nested_map_exception(
             self,
             nested_map: Dict,
@@ -36,3 +36,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Using Try & Testing `access_nested_map`'s """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Testing `get_json` function """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        """ Testing `get_json`'s function & check output"""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
