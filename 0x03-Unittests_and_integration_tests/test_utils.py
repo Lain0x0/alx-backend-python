d7e0723db58a9926700b5d50dd2aa81c2fe968a8
Lain0x0 <lainyamato03@gmail.com>
2024-09-06 15:33:29 +0000

    0x1-test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,17 @@ class TestAccessNestedMap(unittest.TestCase):
             ) -> None:
         """ Testing `access_nested_map`'s output """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """ Using Try & Testing `access_nested_map`'s """
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
