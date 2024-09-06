7cf29599575d360b9ed961d9d4df47c2bff85cb6
Lain0x0 <lainyamato03@gmail.com>
2024-09-06 16:06:18 +0000

    0x4-test_cleint

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,73 +14,73 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ("google", {'login': "google"}),
         ("abc", {'login': "abc"}),
-    ])
+        ])
     @patch(
-        "client.get_json",
-    )
+            "client.get_json",
+            )
     def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
         """ Testing org Method """
         mocked_fxn.return_value = MagicMock(return_value=resp)
         gh_org_client = GithubOrgClient(org)
         self.assertEqual(gh_org_client.org(), resp)
         mocked_fxn.assert_called_once_with(
-            "https://api.github.com/orgs/{}".format(org)
-        )
+                "https://api.github.com/orgs/{}".format(org)
+                )
 
-    def test_public_repos_url(self) -> None:
-        """ Testing public_repos_url prototype """
+        def test_public_repos_url(self) -> None:
+            """ Testing public_repos_url prototype """
         with patch(
                 "client.GithubOrgClient.org",
                 new_callable=PropertyMock,
                 ) as mock_org:
             mock_org.return_value = {
-                'repos_url': "https://api.github.com/users/google/repos",
-            }
+                    'repos_url': "https://api.github.com/users/google/repos",
+                    }
             self.assertEqual(
-                GithubOrgClient("google")._public_repos_url,
-                "https://api.github.com/users/google/repos",
-            )
+                    GithubOrgClient("google")._public_repos_url,
+                    "https://api.github.com/users/google/repos",
+                    )
 
-    @patch("client.get_json")
+            @patch("client.get_json")
     def test_public_repos(self, mock_get_json: MagicMock) -> None:
         """ Testing test_public_repos prototype """
         test_payload = {
-            'repos_url': "https://api.github.com/users/google/repos",
-            'repos': [
-                {
-                    "id": 7697149,
-                    "name": "episodes.dart",
-                    "private": False,
-                    "owner": {
-                        "login": "google",
-                        "id": 1342004,
-                    },
-                    "fork": False,
-                    "url": "https://api.github.com/repos/google/episodes.dart",
-                    "created_at": "2013-01-19T00:31:37Z",
-                    "updated_at": "2019-09-23T11:53:58Z",
-                    "has_issues": True,
-                    "forks": 22,
-                    "default_branch": "master",
-                },
-                {
-                    "id": 8566972,
-                    "name": "kratu",
-                    "private": False,
-                    "owner": {
-                        "login": "google",
-                        "id": 1342004,
-                    },
-                    "fork": False,
-                    "url": "https://api.github.com/repos/google/kratu",
-                    "created_at": "2013-03-04T22:52:33Z",
-                    "updated_at": "2019-11-15T22:22:16Z",
-                    "has_issues": True,
-                    "forks": 32,
-                    "default_branch": "master",
-                },
-            ]
-        }
+                'repos_url': "https://api.github.com/users/google/repos",
+                'repos': [
+                    {
+                        "id": 7697149,
+                        "name": "episodes.dart",
+                        "private": False,
+                        "owner": {
+                            "login": "google",
+                            "id": 1342004,
+                            },
+                        "fork": False,
+                        "url": "https://api.github.com/repos/google/episodes.dart",  # noqa: E501
+                        "created_at": "2013-01-19T00:31:37Z",
+                        "updated_at": "2019-09-23T11:53:58Z",
+                        "has_issues": True,
+                        "forks": 22,
+                        "default_branch": "master",
+                        },
+                    {
+                        "id": 8566972,
+                        "name": "kratu",
+                        "private": False,
+                        "owner": {
+                            "login": "google",
+                            "id": 1342004,
+                            },
+                        "fork": False,
+                        "url": "https://api.github.com/repos/google/kratu",
+                        "created_at": "2013-03-04T22:52:33Z",
+                        "updated_at": "2019-11-15T22:22:16Z",
+                        "has_issues": True,
+                        "forks": 32,
+                        "default_branch": "master",
+                        },
+                    ]
+                }
         mock_get_json.return_value = test_payload["repos"]
         with patch(
                 "client.GithubOrgClient._public_repos_url",
@@ -88,11 +88,21 @@ class TestGithubOrgClient(unittest.TestCase):
                 ) as mock_public_repos_url:
             mock_public_repos_url.return_value = test_payload["repos_url"]
             self.assertEqual(
-                GithubOrgClient("google").public_repos(),
-                [
-                    "episodes.dart",
-                    "kratu",
-                ],
-            )
+                    GithubOrgClient("google").public_repos(),
+                    [
+                        "episodes.dart",
+                        "kratu",
+                        ],
+                    )
             mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
+        ({'license': {'key': "bsl-1.0"}}, "bsd-3-clause", False),
+        ])
+    def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
+        """ Testing has_license prototype """
+        gh_org_client = GithubOrgClient("google")
+        client_has_licence = gh_org_client.has_license(repo, key)
+        self.assertEqual(client_has_licence, expected)
